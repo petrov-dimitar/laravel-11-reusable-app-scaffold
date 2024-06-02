f8dfc1cb44fe4f2c90698bdfe0d8d9ab38f31932
Dimitar Petrov <dimitar.petrov@delasport.com>
2024-06-02 18:18:14 +0300

    Add redux toolkit

---
+++ README.md
@@ -0,0 +1,16 @@
+# Description
+
+## Laravel APP
+- JWT authentication
+- Debug functioanlity with laravel/telescope
+- Users model
+- Middlewares for authentication and authorization via API Key
+
+## React APP
+node -v 21.7.1
+
+- Authentication functionality
+- API service
+- Prettier
+- Eslint
+- Redux Toolkit to handle central app data, making requests to API and caching behaviour
--- react-scaffold/src/modules/authentication/index.jsx
+++ react-scaffold/src/modules/authentication/index.jsx
@@ -1,11 +1,26 @@
 import React from 'react'
 import Login from './Login'
 import Register from './Register'
+import { useGetPokemonByNameQuery } from '../../redux/api.service'
 
 // Define a functional component named MyComponent
 function Authentication() {
+  const { data, error, isLoading } = useGetPokemonByNameQuery('bulbasaur')
+
   return (
     <div>
+      <div className="App">
+        {error ? (
+          <>Oh no, there was an error</>
+        ) : isLoading ? (
+          <>Loading...</>
+        ) : data ? (
+          <>
+            <h3>{data.species.name}</h3>
+            <img src={data.sprites.front_shiny} alt={data.species.name} />
+          </>
+        ) : null}
+      </div>
       <h1>Authentication View</h1>
       <Login />
       <Register />
